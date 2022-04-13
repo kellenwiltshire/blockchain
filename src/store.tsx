3cca6ce935bc0395d1da10640e5beb1e59f07cc6
Kellen Wiltshire <kellenwiltshire@outlook.com>
2022-04-13 10:37:41 -0400

    feat: initial store

---
+++ src/store.tsx
@@ -0,0 +1,33 @@
+import { makeAutoObservable } from 'mobx';
+import { SHA256 } from 'crypto-js';
+import { createContext, useContext, useEffect, FC } from 'react';
+
+interface IBlock {
+	hash: string;
+	transactions: Array<string>;
+}
+
+class BlockchainStore {
+	blocks: Array<IBlock> = [];
+	transactions: Array<string> = [];
+
+	constructor() {
+		makeAutoObservable(this);
+	}
+
+	addTransaction(message: string) {
+		this.transactions.push(message);
+	}
+}
+
+const StoreContext = createContext<BlockchainStore>(new BlockchainStore());
+
+const StoreProvider: FC<{ store: BlockchainStore }> = ({ store, children }) => {
+	return <StoreContext.Provider value={store}>{children}</StoreContext.Provider>;
+};
+
+const useStore = () => {
+	return useContext(StoreContext);
+};
+
+export { BlockchainStore, StoreProvider, useStore };
