af8c4e53533c130c29905030800f0864e86eefaf
Kellen Wiltshire <kellenwiltshire@outlook.com>
2022-04-13 11:27:11 -0400

    feat: block chain completed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,45 @@
 import { useState, FC } from 'react';
 import { observer } from 'mobx-react-lite';
 import { useStore } from '../store';
-import { Heading } from '@chakra-ui/react';
+import { Heading, Input, Button, Text } from '@chakra-ui/react';
 
 const Index: FC = () => {
 	return (
 		<main>
+			<Title />
+			<Form />
 			<Transactions />
+			<Blocks />
 		</main>
+	);
+};
+
+const Title: FC = observer(() => {
+	const store = useStore();
+
+	return (
+		<Heading as='h1'>
+			{store.numberBlocks} Blocks({store.valid ? 'Valid' : 'Invalid'})
+		</Heading>
+	);
+});
+
+const Form: FC = () => {
+	const store = useStore();
+	const [message, setMesssage] = useState('');
+
+	return (
+		<form
+			onSubmit={(e) => {
+				e.preventDefault();
+
+				store.addTransaction(message);
+				setMesssage('');
+			}}
+		>
+			<Input value={message} onChange={(e) => setMesssage(e.target.value)} placeholder='message' required />
+			<Button type='submit'>Add</Button>
+		</form>
 	);
 };
 
@@ -19,13 +51,31 @@ const Transactions: FC = observer(() => {
 			<Heading as='h2' size='2xl'>
 				Pending Transactions
 			</Heading>
-			<ul>
+			<ul className='pending'>
 				{store.transactions.map((transaction, i) => {
 					return <li key={i}>{transaction}</li>;
 				})}
 			</ul>
 		</div>
 	) : null;
+});
+
+const Blocks: FC = observer(() => {
+	const store = useStore();
+
+	return (
+		<div>
+			<Heading>Blocks</Heading>
+			<ul className='blocks'>
+				{[...store.blocks].reverse().map((block) => (
+					<li key={block.hash}>
+						<Heading as='h3'>{block.hash}</Heading>
+						<Text>{JSON.stringify(block.transactions, null, 2)}</Text>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 });
 
 export default Index;
--- src/store.tsx
+++ src/store.tsx
@@ -9,20 +9,58 @@ interface IBlock {
 
 class BlockchainStore {
 	blocks: Array<IBlock> = [];
-	transactions: Array<string> = ['Start'];
+	transactions: Array<string> = [];
 
 	constructor() {
 		makeAutoObservable(this);
 	}
 
+	get numberBlocks() {
+		return this.blocks.length;
+	}
+
+	get valid() {
+		return this.blocks.every((block, index) => {
+			const prevBlock = this.blocks[index - 1] ?? { hash: '' };
+			const hash = SHA256(`${prevBlock.hash}${JSON.stringify(block.transactions)}`).toString();
+
+			return hash === block.hash;
+		});
+	}
+
 	addTransaction(message: string) {
 		this.transactions.push(message);
+	}
+
+	writeBlock() {
+		if (this.transactions.length === 0) {
+			return;
+		}
+
+		const transactions = [...this.transactions];
+		this.transactions = [];
+
+		const prevBlock = this.blocks[this.blocks.length - 1] ?? { hash: '' };
+		const hash = SHA256(`${prevBlock.hash}${JSON.stringify(transactions)}`).toString();
+
+		this.blocks.push({
+			hash,
+			transactions,
+		});
 	}
 }
 
 const StoreContext = createContext<BlockchainStore>(new BlockchainStore());
 
 const StoreProvider: FC<{ store: BlockchainStore }> = ({ store, children }) => {
+	useEffect(() => {
+		const interval = setInterval(() => {
+			store.writeBlock();
+		}, 5000);
+
+		return () => clearInterval(interval);
+	}, [store]);
+
 	return <StoreContext.Provider value={store}>{children}</StoreContext.Provider>;
 };
 
