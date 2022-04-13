e5e21e5278bc5418baf153aef91aa1dc2405c449
Kellen Wiltshire <kellenwiltshire@outlook.com>
2022-04-13 15:40:00 -0400

    chore: extracting components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
-import { useState, FC } from 'react';
-import { observer } from 'mobx-react-lite';
-import { useStore } from '../store';
-import { Heading, Input, Button, Text } from '@chakra-ui/react';
+import { FC } from 'react';
 import { Container } from '../components/Container';
 import { Main } from '../components/Main';
 import { Title } from '../components/Title';
 import { Form } from '../components/Form';
+import { Transactions } from '../components/Transactions';
+import { Blocks } from '../components/Blocks';
 
 const Index: FC = () => {
 	return (
@@ -19,40 +18,5 @@ const Index: FC = () => {
 		</Container>
 	);
 };
-
-const Transactions: FC = observer(() => {
-	const store = useStore();
-
-	return store.transactions.length > 0 ? (
-		<div>
-			<Heading as='h2' size='2xl'>
-				Pending Transactions
-			</Heading>
-			<ul className='pending'>
-				{store.transactions.map((transaction, i) => {
-					return <li key={i}>{transaction}</li>;
-				})}
-			</ul>
-		</div>
-	) : null;
-});
-
-const Blocks: FC = observer(() => {
-	const store = useStore();
-
-	return (
-		<div>
-			<Heading>Blocks</Heading>
-			<ul className='blocks'>
-				{[...store.blocks].reverse().map((block) => (
-					<li key={block.hash}>
-						<Heading as='h3'>{block.hash}</Heading>
-						<Text>{JSON.stringify(block.transactions, null, 2)}</Text>
-					</li>
-				))}
-			</ul>
-		</div>
-	);
-});
 
 export default Index;
